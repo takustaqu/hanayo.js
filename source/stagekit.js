19a122b7eb4f8bffb1aa02d703fd28c91a97132f
Yabu Kiyohide <takustaqu@ow-d.com>
2015-04-23 09:53:28 +0900

    init commit - create main files.

---
+++ source/stagekit.js
@@ -0,0 +1,42 @@
+/*
+|| stagekit.js by [ow;d]
+|| HTML5 canvas API helper
+*/
+
+var stagekit = {}
+
+(function($s){
+
+
+//ステージを定義
+$s.Stage = function(){
+
+}
+
+$s.Stage.prototype.render = function(){
+
+}
+
+
+$s.Casts = function(){
+
+}
+
+$s.Rig = function(){
+
+}
+
+
+/*
+castの仕様
+	source : イメージのソース(puddingとの連携はどうする？)
+	size : [w,h]
+	position : [x,y]
+	translate : [0,0] <- default
+	rotate : 0 <- default
+	scale: 0 
+*/
+
+
+})(stagekit);
+
