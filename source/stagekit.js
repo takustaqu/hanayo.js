2ec0aca5ff922cf310ef1096873622c8ecc64339
Yabu Kiyohide <takustaqu@ow-d.com>
2015-04-26 19:50:03 +0900

    Add rendering cue.  Transform-support: initial position , translate , rotate , opacity   source-support:IMG elements.

--- source/stagekit.js
+++ source/stagekit.js
@@ -8,18 +8,75 @@ var stagekit = {};
 (function($s){
 
 
-//ステージを定義
-$s.Stage = function(){
+function isElement(obj) {
+  try {
+    //Using W3 DOM2 (works for FF, Opera and Chrom)
+    return obj instanceof HTMLElement;
+  }
+  catch(e){
+    //Browsers not supporting W3 DOM2 don't have HTMLElement and
+    //an exception is thrown and we end up here. Testing some
+    //properties that all elements have. (works on IE7)
+    return (typeof obj==="object") &&
+      (obj.nodeType===1) && (typeof obj.style === "object") &&
+      (typeof obj.ownerDocument ==="object");
+  }
+}
 
+
+
+//ステージを定義
+$s.Stage = function(canvasElem){
+	this.canvas = canvasElem;
+	this.canvasSize = [canvasElem.width,canvasElem.height];
+	this.ctx = canvasElem.getContext("2d");
 	this.layers = [];
 	this.casts = {};
 }
 
 $s.Stage.prototype.render = function(){
+	var ctx = this.ctx;
+
+	ctx.clearRect(0, 0, this.canvasSize[0],this.canvasSize[1]);
+
+	for(var i=0,il=this.layers.length; i<il; i++){
+		ctx.save();
+		var cast = this.layers[i];
+
+		//位置指定
+		var tls = [cast.translate[0] + cast.position[0] , cast.translate[0] + cast.position[0]]
+		ctx.translate(tls[0],tls[1]);
+
+		//α
+		ctx.globalAlpha = cast.opacity;
+
+		//回転処理
+		if(!!cast.rotate){
+
+			ctx.translate((cast.size[0]/2) ,  (cast.size[1]/2));
+
+			ctx.rotate(cast.rotate * Math.PI / 180 );
+
+			ctx.translate(-1*(cast.size[0]/2) ,-1*(cast.size[1]/2));
+
+
+		}
+
+		if(cast.sourceType == "image"){
+			ctx.drawImage( cast.source ,
+				0,0, 
+				cast.source.width , cast.source.height ,
+				0,0, 
+				cast.source.width , cast.source.height )
+		}
+
+		ctx.restore();
+	};
+
 }
 
 $s.Stage.prototype.addCasts = function(casts){
-	var i=0,il=0;
+	var i=0 , il=0;
 	if(!!casts){
 
 		for(var i=0,il=casts.length; i<il; i++){
@@ -44,10 +101,32 @@ $s.Stage.prototype.sortLayers = function(){
 	this.casts = {};
 
 	for(var i=0,il=this.layers.length; i<il; i++){
+		this.layers[i] = new $s.Cast(this.layers[i]);
 		console.log(this.layers[i].name)
 		this.casts[this.layers[i].name] = this.layers[i];
 	}
 }
+
+$s.Cast = function(arg){
+	this.name = arg.name;
+	this.size = arg.size;
+	this.position = arg.position;
+	this.layer = arg.layer;
+	this.source = arg.source;
+	this.sourceType = this.detectSourceType(arg.source);
+	this.translate = !!arg.translate ? arg.translate : [0,0] ;
+	this.rotate = !!arg.rotate ? arg.rotate : 0 ;
+	this.opacity = !!arg.opacity ? arg.opacity : 1 ;
+}
+
+$s.Cast.prototype.detectSourceType = function(){
+	if(isElement(this.source)){
+		if(this.source.tagName == "IMG"){
+			return "image"
+		}
+	}
+}
+
 
 $s.Rig = function(){
 }
